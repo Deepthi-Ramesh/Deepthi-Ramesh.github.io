20ebb24f7b76c468dc9a0e32d2f340fef56a31a3
premcloudville <sihtechhunter@gmail.com>
2023-04-19 23:00:06 +0530

    INITIAL COMMIT

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Certifications from "./components/Certifiactions/certifiactions";
+import Port from "./Port";
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Port/>}/>
+        <Route path="/skills" element={<Certifications />} />
+      </Routes>
+    </BrowserRouter>
+    
+    </div>
+  );
+}
+
+export default App;
